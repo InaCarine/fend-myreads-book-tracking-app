7db0752c50aa860a2590f74e09a8b880761dee13
Ina Carine <inakara89@gmail.com>
2018-07-29 10:39:56 +0100

    Make Bookshelf into a func comp

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Book from './Book';
 
-class Bookshelf extends Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired,
-  }
-
-  render() {
-    const shelf = this.props;
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelf.name}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {shelf.books.map(book => (
-              <li key={book.id}>
-                <Book
-                  book={book}
-                  changeShelf={shelf.changeShelf}
-                />
-              </li>
-            ))}
-          </ol>
-        </div>
+const Bookshelf = ({ name, books, changeShelf }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{name}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map(book => (
+            <li key={book.id}>
+              <Book
+                book={book}
+                changeShelf={changeShelf}
+              />
+            </li>
+          ))}
+        </ol>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Bookshelf.propTypes = {
+  name: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  changeShelf: PropTypes.func.isRequired,
+};
 
 export default Bookshelf;
