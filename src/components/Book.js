c24e1b81d89b451477ba4438a06457e21b9e1cf9
Ina Carine <inakara89@gmail.com>
2018-07-29 10:31:33 +0100

    Change Book to be a func comp and fix shelf value

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,44 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Book extends Component {
-  // Make sure that the correct props are being passed in
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    changeShelf: PropTypes.func.isRequired,
-  };
 
+const Book = ({book, changeShelf}) => {
   /*
   * @description: This function gets called when a shelf have been selected from the drop down list
   * It passes back the book and the selected shelf to the main method in App.js
   */
-  handleSelectChange = (event) => {
-    if(event.target.value === this.props.book.shelf) return;    
-    if(this.props.changeShelf) this.props.changeShelf(this.props.book, event.target.value);    
+  const handleSelectChange = event => {
+    if (event.target.value === book.shelf) return;
+    if (changeShelf) changeShelf(book, event.target.value);
   };
 
-  render() {
-    const book = this.props.book;
-    
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-            <select value={book.shelf} onChange={this.handleSelectChange}>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
+        <div className="book-shelf-changer">
+          <select value={book.shelf || 'none'} onChange={handleSelectChange}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
       </div>
-    );
-  }
-}
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors}</div>
+    </div>
+  );
+};
+
+// Make sure that the correct props are being passed in
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  changeShelf: PropTypes.func.isRequired,
+};
 
 export default Book;
